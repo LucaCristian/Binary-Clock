77c667092b8335c5a3e1aa885bb188c4bbc8d407
Luca Cristian <thelucutz@gmail.com>
2022-11-30 14:11:30 +0200

    Add project files.

---
+++ Binary Clock/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Clock
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Clock");
+            Console.Write("\n");
+
+            Console.Write(Convert.ToString(System.DateTime.Now.Hour, 2));
+            Console.Write(":");
+
+            Console.Write(Convert.ToString(System.DateTime.Now.Minute, 2));
+            Console.Write(":");
+
+            Console.Write(Convert.ToString(System.DateTime.Now.Second, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
